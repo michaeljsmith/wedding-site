ed61a3d6a1d8a48a48ebed254ee31e59a13fbae6
Michael Smith <msmith@msmith.id.au>
2014-02-22 10:32:04 +1100

    Progress.

---
+++ style.js
@@ -0,0 +1,13 @@
+      // this will be called onload
+      $(document).ready(function() {
+        console.log("got here 0");
+        jQuery(function($) {
+            var id = 0;
+            $('.photo-gallery img').each(function() {
+              id += 1;
+              var a = 15 * (Math.random() * 2 - 1);
+              console.log("got here " + id + " " + a);
+              //$(this).css('transform', 'rotate(' + a + 'deg)');
+            });
+        });
+      });
